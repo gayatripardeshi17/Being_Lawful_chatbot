a5a9c3988e66f869785a0767ef74b070723ac40d
Gayatri Devendrasinh Pardeshi <149011159+gayatripardeshi17@users.noreply.github.com>
2025-06-20 13:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
